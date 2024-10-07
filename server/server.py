d226c94ac62bb3d73b83aa721d097729cc40db1a
Anupta <anupta22@gmail.com>
2024-10-06 20:34:38 -0400

    Created separate server file

---
+++ server/server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# server.py
+# Author: Anupta Argo
+#-----------------------------------------------------------------------
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+#-----------------------------------------------------------------------
+
+PORT = 8080
+
+#-----------------------------------------------------------------------
+
+# app instance
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/home', methods=['GET'])
+def return_home():
+    return jsonify({
+        'Question': "Question text lorem ipsum dolor",
+        'Img': 'imgblob'
+    })
+
+#-----------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run(debug=True, port=PORT)
